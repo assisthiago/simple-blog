8a23fb6ca10091e47dd8c31b59129a340df7a01f
Thiago Assis <thiago.medassis@gmail.com>
2022-03-21 22:29:44 -0300

    Core

--- app/urls.py
+++ app/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import app.core.views
 
 urlpatterns = [
+    path('', app.core.views.home),
     path('admin/', admin.site.urls),
 ]
